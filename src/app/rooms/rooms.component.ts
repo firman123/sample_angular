2978d36455130e694d888021317431c2e09772bf
ats ardzi <ats-ardzi@ioh.co.id>
2023-06-06 14:23:46 +0700

    Learn directive

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css'],
+})
+export class RoomsComponent implements OnInit {
+  hotelName = 'Hilton Hotel';
+  numberOfRooms = 10;
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomList[] = [
+    {
+      roomType: 'Ekonomi',
+      amenities: 'Sample',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+    },
+    {
+      roomType: 'Ekonomi',
+      amenities: 'Sample',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+    },
+    {
+      roomType: 'Ekonomi',
+      amenities: 'Sample',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+    },
+    {
+      roomType: 'Ekonomi',
+      amenities: 'Sample',
+      price: 1000,
+      photos:
+        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+      checkinTime: new Date('11-Nov-2022'),
+      checkoutTime: new Date('12-Nov-2022'),
+    },
+  ];
+
+  ngOnInit(): void {}
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+}
