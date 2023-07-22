997186ac5137289a00ae7118908eab3aedbeab4e
ats ardzi <ats-ardzi@ioh.co.id>
2023-07-22 07:44:11 +0700

    latihan add component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -11,54 +11,62 @@ export class RoomsComponent implements OnInit {
   numberOfRooms = 10;
   hideRooms = false;
 
+  selectedRoom!: RoomList;
+
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomType: 'Ekonomi',
-      amenities: 'Sample',
-      price: 1000,
-      photos:
-        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-    },
-    {
-      roomType: 'Ekonomi',
-      amenities: 'Sample',
-      price: 1000,
-      photos:
-        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-    },
-    {
-      roomType: 'Ekonomi',
-      amenities: 'Sample',
-      price: 1000,
-      photos:
-        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-    },
-    {
-      roomType: 'Ekonomi',
-      amenities: 'Sample',
-      price: 1000,
-      photos:
-        'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
-      checkinTime: new Date('11-Nov-2022'),
-      checkoutTime: new Date('12-Nov-2022'),
-    },
-  ];
+  roomList: RoomList[] = [];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomType: 'Ekonomi',
+        amenities: 'Sample',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+      },
+      {
+        roomType: 'Ekonomi',
+        amenities: 'Sample',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+      },
+      {
+        roomType: 'Ekonomi',
+        amenities: 'Sample',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+      },
+      {
+        roomType: 'Ekonomi',
+        amenities: 'Sample',
+        price: 1000,
+        photos:
+          'https://images.unsplash.com/photo-1685572699541-00ccdc31b0ef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=363&q=80',
+        checkinTime: new Date('11-Nov-2022'),
+        checkoutTime: new Date('12-Nov-2022'),
+      },
+    ];
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
   }
 }
