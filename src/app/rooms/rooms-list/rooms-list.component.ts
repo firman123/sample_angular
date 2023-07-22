b25e38aa9efaeb225ab4e2c27f7122f95069f993
ats ardzi <ats-ardzi@ioh.co.id>
2023-07-22 07:50:26 +0700

    no message

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -11,10 +11,7 @@ export class RoomsListComponent {
   @Output() selectedRoom = new EventEmitter<RoomList>();
   selectRoom(room: RoomList) {
     this.selectedRoom.emit(room);
-    //ini comment 1
-    //ini comment 2
-    //ini comment 3
-    //ini comment 4
-    //
+
+    //add comment
   }
 }
